7604a69d7e6335e5cec3809c8273a15dbec9c282
oliverjhernandez <oliverjhernandez@gmail.com>
2022-06-24 12:01:37 -0500

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import * as core from '@actions/core'
+
+async function run(): Promise<void> {
+    try {
+        const branchName = core.getInput('branch_name')
+        console.log(`Sanitizing: ${branchName}`)
+        const sanitizedBranchName = branchName
+            .trim()
+            .toLowerCase()
+            .replace(/[\W_]+/g, '-')
+            .substring(0, 16)
+        core.exportVariable("BRANCH_NAME", sanitizedBranchName)
+    } catch (error) {}
+}
+
+run()
